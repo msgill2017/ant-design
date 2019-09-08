38d8b7995a5c125eaee611d66342831749ed4976
msgill2017 <35434680+msgill2017@users.noreply.github.com>
2019-09-08 14:28:27 -0400

    Punjabi (India) locale

---
+++ components/locale/pb_IN.tsx
@@ -0,0 +1,66 @@
+import Pagination from 'rc-pagination/lib/locale/pb_IN';
+import DatePicker from '../date-picker/locale/pb_IN';
+import TimePicker from '../time-picker/locale/pb_IN';
+import Calendar from '../calendar/locale/pb_IN';
+
+export default {
+  locale: 'pb',
+  Pagination,
+  DatePicker,
+  TimePicker,
+  Calendar,
+  // locales for all comoponents
+  global: {
+    placeholder: 'ਕਿਰਪਾ ਕਰਕੇ ਚੁਣੋ',
+  },
+  Table: {
+    filterTitle: 'ਬੰਦ ਸੂਚੀ',
+    filterConfirm: 'ਚੰਗਾ',
+    filterReset: 'ਰੀਸੈੱਟ',
+    emptyText: 'ਕੋਈ ਜਾਣਕਾਰੀ ਨਹੀਂ',
+    selectAll: 'ਮੌਜੂਦਾ ਪੰਨਾ ਚੁਣੋ',
+    selectInvert: 'ਮੌਜੂਦਾ ਪੇਜ ਨੂੰ ਘੁੰਮਾਓ',
+    sortTitle: 'ਕ੍ਰਮਬੱਧ',
+    expand: 'ਕਤਾਰ ਫੈਲਾਓ',
+  },
+  Modal: {
+    okText: 'ਠੀਕ',
+    cancelText: 'ਰੱਦ ਕਰੋ',
+    justOkText: 'ਠੀਕ',
+  },
+  Popconfirm: {
+    okText: 'ਠੀਕ',
+    cancelText: 'ਰੱਦ ਕਰੋ',
+  },
+  Transfer: {
+    titles: ['', ''],
+    notFoundContent: 'ਨਹੀਂ ਮਿਲਿਆ',
+    searchPlaceholder: 'ਇੱਥੇ ਲੱਭੋ',
+    itemUnit: 'ਆਈਟਮ',
+    itemsUnit: 'ਆਈਟਮਾਂ',
+  },
+  Select: {
+    notFoundContent: 'ਨਹੀਂ ਮਿਲਿਆ',
+  },
+  Upload: {
+    uploading: 'ਅਪਲੋਡ ਕਰ ਰਿਹਾ ਹੈ...',
+    removeFile: 'ਫਾਈਲ ਹਟਾਓ',
+    uploadError: 'ਅਪਲੋਡ ਗਲਤੀ',
+    previewFile: 'ਪ੍ਰੀਵਿview ਫਾਈਲ',
+  },
+  Empty: {
+    description: 'ਕੋਈ ਡਾਟਾ ਨਹੀਂ',
+  },
+  Icon: {
+    icon: 'ਆਈਕਾਨ',
+  },
+  Text: {
+    edit: 'ਸੋਧ',
+    copy: 'ਨਕਲ',
+    copied: 'ਨਕਲ ਕੀਤਾ',
+    expand: 'ਫੈਲਾਓ',
+  },
+  PageHeader: {
+    back: 'ਵਾਪਸ',
+  },
+};
